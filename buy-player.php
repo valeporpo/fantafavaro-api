1bb44a50a8ef603e930e9e0ec0bae15593853a59
Valerio <valerio.porpo@gmail.com>
2022-08-25 14:30:07 +0200

    Db connection

--- buy-player.php
+++ buy-player.php
@@ -1,3 +1,9 @@
 <?php
  echo 'ciao';
+ $dbconn = pg_connect("host=ec2-54-228-125-183.eu-west-1.compute.amazonaws.com
+                       dbname=d6memn437ahh1b
+                       user=uqkswzeelyfzxo
+                       password=13e44cbf86a3cd6b19c584aee01cf90c97493aadf366fc54e1f67eebb6e13a15")
+    or die('Could not connect: ' . pg_last_error());
+ print_r($dbconn);   
 ?>
